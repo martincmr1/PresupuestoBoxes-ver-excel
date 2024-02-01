974d280a901374d3281d9857751daa82f8f96302
martincmr1 <martincmr1@hotmail.com>
2024-01-31 22:18:36 -0300

    310120244

--- js/index.js
+++ js/index.js
@@ -2103,6 +2103,8 @@ document.getElementById("autos").addEventListener("change", function () {
     campo5.value = "15442";
     campo6.value = "15420";
     campo7.value = "18726";
+
+    UpdateQuantity("2", "cantidad2");
   }
   if (valor_select == "civicvtec") {
     campo1.value = "170995";
